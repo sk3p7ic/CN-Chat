b68cfce864703c2d6ffe5249701b392eb7c8423c
sk3p7ic <joshuaibrom@gmail.com>
2021-10-26 16:47:37 -0500

    Worked on type checking and checking that a server is running before user is transferred

--- src/server/ServerPool.py
+++ src/server/ServerPool.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 import socket
 
-from server.auth.Exceptions import InvalidMemberError
+from server.auth.Exceptions import InvalidMemberError, ServerNotStartedError
 
 
 @dataclass
@@ -21,11 +21,14 @@ class ServerPool:
 
 
     def add_server(self, server_name, is_public, clients=None):
+        # Check inputs to make sure that they are using the correct types
+        if type(is_public) is not bool:
+            raise TypeError(f"Type of 'is_public' is not dict. (Currently {type(is_public)}).")
+        if clients is not None and type(clients) is not dict:
+            raise TypeError(f"Type of 'clients' is not dict. (Currently {type(clients)}).")
         # Check if server is set to private mode and if both clients have been given if the server is private
         if not is_public and (c_len := len(clients)) != 2:
             raise ValueError(f"Server is set to private and does not have 2 clients. (Supplied {c_len}).")
-        if clients is not None and type(clients) is not dict:
-            raise TypeError(f"Type of 'clients' is not dict. (Currently {type(clients)}).")
         new_server_id = sorted(servers)[-1] + 1  # Get last highest ID and add one
         # Add the server information to the database
         self.cursor.execute("INSERT INTO servers (server_id, server_name) VALUES (?, ?)",
@@ -47,12 +50,16 @@ class ServerPool:
         server_info = None
         try:
             server_info = self.servers[server_id]
-            if client_id not in server_info.get("clients"):  # If the client is not listed as a member of server
+            # If the client is not listed as a member of a private server
+            if not bool(server_info.get("is_public")) and client_id not in server_info.get("clients"):
                 raise InvalidMemberError(client_id, server_id)
         except KeyError:
             raise KeyError(f"Server ID, '{server_id}', is not a valid server ID.")
         if client_id not in self.clients:
             raise KeyError(f"Client with client ID '{client_id}' has not been registered to the server pool.")
+        # Ensure that the server has been created / is running
+        if server_id not in self.servers:
+            raise ServerNotStartedError(server_id)
         # Add the client to the server
         self.servers[server_id]["server_class"].add_client(client_id, self.clients[client_id])
         # TODO: Check if server is running and transfer client socket to that server
