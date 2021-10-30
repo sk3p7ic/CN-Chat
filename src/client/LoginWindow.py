7162c70b8f52cde7a47ddc4909406ef80a10211a
sk3p7ic <joshuaibrom@gmail.com>
2021-10-30 16:24:11 -0500

    Worked on client-side operations for logging in.

--- src/client/LoginWindow.py
+++ src/client/LoginWindow.py
@@ -1,9 +1,17 @@
+import configparser
+import os.path
+import hashlib
 import tkinter as tk
 from time import sleep
 
+from common.RequestStructures import *
+
+
 class LoginWindow(tk.Frame):
-    def __init__(self, tk_root):
+    def __init__(self, tk_root, config, server):
         self.root = tk_root
+        self.config = config
+        self.server = server
         super().__init__(tk_root)
         self.pack()
         # Create the labels
@@ -30,19 +38,56 @@ class LoginWindow(tk.Frame):
         """Called when the user clicks the submit button."""
         self.username = self.tk_username.get()
         self.password = self.tk_password.get()
+        is_authenticated = self.attempt_validation()
+        # TODO: Destroy this window if True
         self.root.destroy()
+
+    def attempt_validation(self):
+        token, user_id, response_code = self.get_token()
+        if response_code == 0:
+            message = Message(user_id, bytes(token, "utf8"), MsgTypes.MSG_NAME)
+            # Send the json string to the server
+            self.server.send(bytes(message.get_json_str(), "utf8"))
+            print("sent")
+            data = self.server.recv(BUFF_SIZE)
+            server_msg = get_message_from_json(data.decode("utf8"))  # Decode and get the Message that was recieved
+            if get_type_from_str(server_msg.get_json()["msg_type"]) is MsgTypes.MSG_PASS:
+                print("We were logged in")
+                return True
+        return False
 
     def get_inputs(self):
         """Returns the inputs that the user gave for ther username and password"""
         return self.username, self.password
 
+    def get_token(self):
+        tokenfile_path = self.config["DEFAULT"]["tokenfile_location"]
+        print(tokenfile_path)
+        if os.path.exists(tokenfile_path):
+            with open(tokenfile_path, 'r') as tokenfile:
+                contents = tokenfile.read()
+                if len(contents) == 0:  # If there's nothing in the file
+                    print("ERROR: There is nothing in the token file!")
+                    return None, -1, 2
+                lines = contents.split('\n')  # Get the lines of the file
+                if lines[-1] == '': del lines[-1]  # Remove the last line if it's empty
+                if (line_length := len(lines)) != 2:
+                    print(f"ERROR: Length of lines is not 2 (currently {line_length})!")
+                    return None, -1, 3
+                username, password, user_id = lines[0].split(' ')
+                if username == self.username and password == hashlib.md5(self.password.encode()).hexdigest():
+                    return lines[1], user_id, 0  # This line will contain the token
+        else:
+            print("ERROR: Tokenfile does not exist!")
+            return None, -1, 1
 
-def start(root=None):
+
+def start(config, server, root=None):
     # Create the root window if it's not given
     if root is None:
         root = tk.Tk()
     root.title("CN-Chat Client :: Login")  # Set the title
-    login_window = LoginWindow(root)  # Start the app and wait for user input
+    login_window = LoginWindow(root, config, server)  # Start the app and wait for user input
     username, password = login_window.get_inputs()  # Get what the user input
     # TODO: Validate before killing login window
     return username, password  # Return the username and password that the user gave
